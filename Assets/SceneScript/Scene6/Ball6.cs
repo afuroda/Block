0a06bab0adebb5aeb2ef667473e15e7341ac5f04
koki <iosgitapp@gmail.com>
2019-09-06 17:25:53 +0900

    2019/9/7
    blockの形を変える前にセーブ

---
+++ Assets/SceneScript/Scene6/Ball6.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball6 : MonoBehaviour {
+    private float speed = 2000.0f;
+
+    public static Rigidbody Egg6;
+
+    public float moveForceMultiplier;
+    // Start is called before the first frame update
+    void Start () {
+        this.GetComponent<Rigidbody> ().AddForce (
+            (transform.forward + transform.right) * speed);
+
+        Egg6 = this.GetComponent<Rigidbody> ();
+    }
+
+    void OnCollisionEnter (Collision collisionInfo) {
+        this.GetComponent<Rigidbody> ().velocity = this.GetComponent<Rigidbody> ().velocity.normalized * 30;
+
+        if (this.GetComponent<Rigidbody> ().position.x == 4.5) {
+            this.GetComponent<Rigidbody> ().velocity = new Vector3 (-5f, 0f, -5f);
+        }
+        if (this.GetComponent<Rigidbody> ().position.x == -4.5) {
+            this.GetComponent<Rigidbody> ().velocity = new Vector3 (5f, 0f, 5f);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () { }
+}
