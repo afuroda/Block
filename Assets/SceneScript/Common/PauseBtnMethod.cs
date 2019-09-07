02d832fe4a124a56272d613ab38c27f083c1a332
koki <iosgitapp@gmail.com>
2019-09-07 22:33:37 +0900

    2019/9/7
    一時停止ボタン作成

---
+++ Assets/SceneScript/Common/PauseBtnMethod.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseBtnMethod : MonoBehaviour {
+
+    private Image btnImage;
+
+    public Sprite PauseSprite;
+
+    public Sprite PlaySprite;
+
+    public static bool PauseFlag;
+
+    public Rigidbody PauseRigidbody;
+
+    public Vector3 PauseVelocity;
+    // Start is called before the first frame update
+    void Start () {
+
+        PauseFlag = false;
+        btnImage = GameObject.Find ("Canvas/PauseBtn").GetComponent<Image> ();
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    public void changeSprite () { // 必ず public にする
+
+        if (PauseFlag == false) {
+
+            PauseRigidbody = Ball.Egg.GetComponent<Rigidbody> ();
+            PauseVelocity = Ball.Egg.GetComponent<Rigidbody> ().velocity;
+
+            PauseRigidbody.velocity = Vector3.zero;
+
+            btnImage.sprite = PlaySprite;
+            PauseFlag = true;
+        } else if (PauseFlag == true) {
+
+            PauseRigidbody.velocity = PauseVelocity;
+
+            btnImage.sprite = PauseSprite;
+            PauseFlag = false;
+        }
+
+    }
+}
